bad5f5fdad4d81200956232fc71dfa1976412d03
Amin Bagheri <amin.bagheri@gmail.com>
2022-08-25 11:45:03 -0400

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by Amin  Bagheri  on 2022-08-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TwitterCloneUITests/TwitterCloneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TwitterCloneUITestsLaunchTests.swift
+//  TwitterCloneUITests
+//
+//  Created by Amin  Bagheri  on 2022-08-25.
+//
+
+import XCTest
+
+class TwitterCloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
